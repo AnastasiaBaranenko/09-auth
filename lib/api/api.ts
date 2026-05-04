0ce4d4ea1258b21d05c7ad60931f00a3d3227365
Anastasia Baranenko <anastasiyabaranenko78@gmail.com>
2026-05-04 14:30:55 +0300

    add log

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
-console.log("CLIENT SIDE API URL:", process.env.NEXT_PUBLIC_API_URL)
 
 export const nextServer = axios.create({
   baseURL: baseURL,
   withCredentials: true,
   });
 
+  console.log("API:", process.env.NEXT_PUBLIC_API_URL + '/api')
 
   
